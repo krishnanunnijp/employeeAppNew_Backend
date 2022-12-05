6a854265daeb95d901a4a028d7c08d887f1648fe
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-05 22:30:11 +0530

    Initial commit

---
+++ src/main/java/com/example/employeeAppNew_Backend/EmployeeAppNewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeAppNew_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppNewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppNewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeeAppNew_Backend/EmployeeAppNewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeAppNew_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAppNewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
