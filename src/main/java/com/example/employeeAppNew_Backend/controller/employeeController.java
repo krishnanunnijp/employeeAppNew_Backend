02e3f710f2c0aac3932047f2f5922bdb9b179ad8
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-06 14:47:17 +0530

    dvj

--- src/main/java/com/example/employeeAppNew_Backend/controller/employeeController.java
+++ src/main/java/com/example/employeeAppNew_Backend/controller/employeeController.java
@@ -1,9 +1,6 @@
 package com.example.employeeAppNew_Backend.controller;
 
-import com.example.employeeAppNew_Backend.model.employee;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,26 +9,23 @@ public class employeeController {
     public String welcome() {
         return "welcome to welcome page";
     }
-
-
-    @PostMapping(path = "/add", consumes = "application/json",produces = "application/json")
-    public String add(@RequestBody employee e ) {
-        System.out.println(e.toString());
+    @GetMapping("/add")
+    public String add() {
         return "add employee page";
     }
-    @PostMapping("/search")
+    @GetMapping("/search")
     public String search() {
         return "welcome to search employee page";
     }
-    @PostMapping("/view")
+    @GetMapping("/view")
     public String view() {
         return "welcome to view employee page";
     }
-    @PostMapping("/edit")
+    @GetMapping("/edit")
     public String edit() {
         return "welcome to edit employee page";
     }
-    @PostMapping("/delete")
+    @GetMapping("/delete")
     public String delete() {
         return "welcome to delete employee page";
     }
