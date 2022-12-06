5b64de368589bdecd8e36fc722cb9bbb73c72b2a
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-06 10:35:54 +0530

    post page

--- src/main/java/com/example/employeeAppNew_Backend/controller/employeeController.java
+++ src/main/java/com/example/employeeAppNew_Backend/controller/employeeController.java
@@ -1,7 +1,9 @@
 package com.example.employeeAppNew_Backend.controller;
 
+import com.example.employeeAppNew_Backend.model.employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +12,11 @@ public class employeeController {
     public String welcome() {
         return "welcome to welcome page";
     }
-    @PostMapping("/add")
-    public String add() {
+
+
+    @PostMapping(path = "/add", consumes = "application/json",produces = "application/json")
+    public String add(@RequestBody employee e ) {
+        System.out.println(e.toString());
         return "add employee page";
     }
     @PostMapping("/search")
